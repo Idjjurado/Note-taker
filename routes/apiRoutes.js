fb536d0a6668347a01f53b7af297d09f28cb3dba
idjjurado <ismael@eduphoria.net>
2023-07-18 23:10:19 -0600

    finished edding not working routes as well as adding prettier to format code!

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,12 @@
+// will be adding lots of notes since this was the most difficult to understand
 const router = require("express").Router();
 const store = require("../db/store");
 
 // this router gets all notes from the db.json file
 router.get("/notes", (req, res) => {
+  // this is the getNotes function from store.js
   store
+  // this returns all notes from the db.json file
     .getNotes()
     .then((notes) => {
       return res.json(notes);
@@ -13,16 +16,20 @@ router.get("/notes", (req, res) => {
 
 // this router adds a new note to the db.json file
 router.post("/notes", (req, res) => {
+  // this is the addNote function from store.js
   store
-    .addNote()
+  // req.body is the new note that is being added
+    .addNote(req.body)
     .then((note) => res.json(note))
     .catch((err) => res.status(500).json(err));
 });
 
 // this router deletes the note with an id equal to req.params.id
 router.delete("/notes/:id", (req, res) => {
+  // this is the removeNote function from store.js
   store
-    .removeNote()
+  // req.params.id is the id of the note that is being deleted
+    .removeNote(req.params.id)
     .then(() => res.json({ ok: true }))
     .catch((err) => res.status(500).json(err));
 });
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,11 +1,15 @@
 const path = require('path');
 const router = require('express').Router();
 
+// this router gets the notes.html file
 router.get('/notes', (req, res) => {
+    // this sends the notes.html file to the client
     res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
+// this router gets the index.html file
 router.get('*', (req, res) => {
+    // this sends the index.html file to the client
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
